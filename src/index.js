1d95bfa6e69f8238819bd866c9d2684c41e73cbe
Konrad Swic <Konrad.Swic.ctr@sabre.com>
2019-01-03 17:25:57 +0100

    Second commit

---
+++ src/index.js
@@ -0,0 +1,28 @@
+const { GraphQLServer } = require('graphql-yoga');
+
+
+let links = [{
+    id: 'link-0',
+    url: 'www.howtographql.com',
+    description: 'Fullstack tutorial for GraphQL'
+}]
+
+const resolvers = {
+    Query: {
+        info: () => null,
+        feed: () => links,
+    },
+    Link: {
+        id: (parent) => parent.id,
+        description: (parent) => parent.description,
+        url: (parent) => parent.url,
+    }
+}
+
+const server = new GraphQLServer({
+    typeDefs: './schema.graphql',
+    resolvers
+})
+server.start(() => console.log(`Server is running on http://localhost:4000`))
+
+
